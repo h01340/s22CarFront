c94a2dff071d111ba47e48b92fb0159e3fff30a8
Pellikka Minna <h01340@haaga-helia.fi>
2022-11-08 15:43:58 +0200

    cors configurations

---
+++ src/components/CarList.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react"
+
+export default function Carlist() {
+    const [cars, setCars] = useState([])
+    useEffect(() => fetchData, [])
+
+
+
+    const fetchData = () => {
+        fetch('http://localhost:8080/rest/cars')
+            .then(response => response.json())
+            .then(responseData =>
+                setCars(responseData._embedded.cars))
+    }
+    return (
+        <div></div>
+    )
+}
